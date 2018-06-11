ff0a7a8f6db48e3962ed179b2642a826ec7ba106
maric-matej <33515600+maric-matej@users.noreply.github.com>
2018-06-12 00:40:26 +0200

    hero-imgs

--- useful-links.php
+++ useful-links.php
@@ -12,7 +12,7 @@ get_header();
 
         
         <main>
-            <section class="hero-index" style="background-image:url('<?php echo get_template_directory_uri(); ?>/images/hero-pula.jpg');">
+            <section class="hero-index" style="background-image:url('<?php echo get_template_directory_uri(); ?>/images/hero-trunk.jpg');">
                 
             </section>
         </main>
